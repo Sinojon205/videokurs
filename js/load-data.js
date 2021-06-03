29556aaa80b2edfbe86f9714b4242cf7c87d2eae
manzura <manzuraihamzaali@gmail.com>
2021-06-03 19:36:01 +0300

    Stilro digargun kardam.

--- js/load-data.js
+++ js/load-data.js
@@ -33,28 +33,28 @@ function renderFakultetPopup() {
         div.appendChild(img);
         div.appendChild(h1);
         div.onclick = () => {
-            var kafs=kafedras.filter((it)=>it.id_fakultet===item.id);
-            renderKafedraPopup(kafs,popup);
+            var kafs = kafedras.filter((it) => it.id_fakultet === item.id);
+            renderKafedraPopup(kafs, popup);
         }
         popup.appendChild(div);
     });
 }
 
-function renderKafedraPopup(kafedraho,popup){
-popup.innerHTML="";
-kafedraho.forEach((item) => {
-    var h1 = document.createElement('h1');
-    h1.innerHTML = item.name;
-    var img = document.createElement('img');
-    img.src = "assets/images/" + (item.rasm?item.rasm:"green.jpg");
-    var div = document.createElement('div');
-    div.setAttribute('class', 'item');
-    div.setAttribute('id', item.id);
-    div.appendChild(img);
-    div.appendChild(h1);
-    div.onclick = () => {
-        console.log(item.id)
-    }
-    popup.appendChild(div);
-});
+function renderKafedraPopup(kafedraho, popup) {
+    popup.innerHTML = "";
+    kafedraho.forEach((item) => {
+        var h1 = document.createElement('h1');
+        h1.innerHTML = item.name;
+        var img = document.createElement('img');
+        img.src = "assets/images/" + (item.rasm ? item.rasm : "green.jpg");
+        var div = document.createElement('div');
+        div.setAttribute('class', 'item');
+        div.setAttribute('id', item.id);
+        div.appendChild(img);
+        div.appendChild(h1);
+        div.onclick = () => {
+            console.log(item.id)
+        }
+        popup.appendChild(div);
+    });
 }
